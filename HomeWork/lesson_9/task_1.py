370cab81f61a064ab65a011d754fb1606254f901
pa.remizov <pa.remizov@tensor.ru>
2025-12-26 15:38:52 +0300

    leson_9

---
+++ HomeWork/lesson_9/task_1.py
@@ -0,0 +1,22 @@
+# Дан текстовый файл test_file/task1_data.txt
+# Он содержит текст, в словах которого есть цифры.
+# Необходимо удалить все цифры и записать получившийся текст в файл test_file/task1_answer.txt
+import re
+
+with open('test_file/task1_data.txt', 'r', encoding='utf-8') as f1:
+    text = f1.read()
+
+new_text = re.sub(r'\d+', '', text)
+
+with open('test_file/task1_answer.txt', 'w', encoding='utf-8') as f2:
+    f2.write(new_text)
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+with open("test_file/task1_answer.txt", 'r', encoding='utf-8') as file1:
+    with open("test_file/task1_ethalon.txt", 'r', encoding='utf-8') as file2:
+        answer = file1.readlines()
+        ethalon = file2.readlines()
+        assert answer == ethalon, "Файл ответа не совпадает с эталонном"
+print('Всё ок')
